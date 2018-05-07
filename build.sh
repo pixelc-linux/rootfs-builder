e2eed21e861f57a65a760312c77f747fa15eaa75
Denys Vitali <denys@denv.it>
2018-05-07 22:52:59 +0200

    Update build and Arch

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+#set -e
+#. ./env.sh
+
 export ARCH=aarch64
+export RFS_WIFI_SSID=$WIFI_SSID
+export RFS_WIFI_PASSWORD=$WIFI_PASSWORD
 
 if [ -z "$DISTRO" ]; then
   echo "Distro not set";
+  exit
+fi
+
+if [ -z "$TOP" ]; then
+  echo "TOP not set"
   exit
 fi
 
@@ -28,4 +38,22 @@ if [ ! -d "$SYSROOT" ]; then
   exit
 fi
 
+sudo chown -R $UID:$GID $SYSROOT
+
 distros/$DISTRO/build.sh
+
+# Chmod
+sudo chown -R 0:0 $SYSROOT/
+sudo chown -R 1000:1000 $SYSROOT/home/alarm
+
+sudo chmod +s $SYSROOT/usr/bin/chfn
+sudo chmod +s $SYSROOT/usr/bin/newgrp
+sudo chmod +s $SYSROOT/usr/bin/passwd
+sudo chmod +s $SYSROOT/usr/bin/chsh
+sudo chmod +s $SYSROOT/usr/bin/gpasswd
+sudo chmod +s $SYSROOT/bin/umount
+sudo chmod +s $SYSROOT/bin/mount
+sudo chmod +s $SYSROOT/bin/su
+
+cd $SYSROOT
+sudo tar -cvpzf $TOP/out/rootfs.tar.gz .
--- distros/arch/build.sh
+++ distros/arch/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source ../../build.sh
 DISTRO_NAME="Arch Linux ARM"
 
 echo -e '
@@ -14,12 +14,12 @@ echo -e '
    \e[1;36m/.^         ^.\ \e[0;37mTM"
 '
 
-if [ ! -z "$WIFI_SSID" ]; then
+if [ -z ${RFS_WIFI_SSID+x} ]; then
   echo "WIFI SSID not set! Using 'Pixel C'";
   WIFI_SSID="Pixel C"
 fi
 
-if [ ! -z "$WIFI_PASSWORD" ]; then
+if [ -z ${RFS_WIFI_PASSWORD+x} ]; then
   echo "WIFI Password not set! Using 'connectme!'";
   WIFI_PASSWORD="connectme!"
 fi
@@ -62,15 +62,15 @@ xorg-server
 onboard 
 bluez 
 bluez-tools 
+bluez-utils
 openbox 
 sudo 
+kitty
 netctl
 wpa_supplicant
 dhcpcd
 dialog 
 networkmanager"
-
-echo $packages
 
 e_status "Installing packages..."
 run_in_qemu pacman -Syu --needed --noconfirm $packages
@@ -88,7 +88,7 @@ sed -i 's/#keyboard=/keyboard=onboard/' $SYSROOT/etc/lightdm/lightdm-gtk-greeter
 
 e_status "Adding Wi-Fi connection"
 mkdir -p $SYSROOT/etc/NetworkManager/system-connection/
-echo > $SYSROOT/etc/NetworkManager/system-connection/wifi-conn-1 <<EOF
+cat > $SYSROOT/etc/NetworkManager/system-connection/wifi-conn-1 <<EOF
 [connection]
 id=wifi-conn-1
 uuid=4f1ca129-1d42-4b8b-903f-591640da4015
@@ -112,4 +112,60 @@ dns-search=
 method=auto
 EOF
 
+if [ -z "$KBD_BT_ADDR" ]; then
+  e_status "Configuring BT Keyboard"
+  
+  cat > $SYSROOT/etc/btkbd.conf <<EOF
+BTKBDMAC = ''$KBD_BT_ADDR''
+EOF
+  e_status "=> Adding BT Keyboard service"
+
+  cat > $SYSROOT/etc/systemd/system/btkbd.service <<EOF
+[Unit]
+Description=systemd Unit to automatically start a Bluetooth keyboard
+Documentation=https://wiki.archlinux.org/index.php/Bluetooth_Keyboard
+ConditionPathExists=/etc/btkbd.conf
+ConditionPathExists=/usr/bin/bluetoothctl
+
+[Service]
+Type=oneshot
+EnvironmentFile=/etc/btkbd.conf
+ExecStart=/usr/bin/bluetoothctl connect ${BTKBDMAC}
+
+[Install]
+WantedBy=bluetooth.target
+EOF
+  run_in_qemu systemctl enable btkbd
+fi
+
+if [ ! -z "$KB_LAYOUT" -o -! -z "$KB_MAP" ]; then
+  KB_LAYOUT = "ch"
+  KB_MAP = "de"
+fi
+
+cat > /etc/X11/xorg.conf.d/00-keyboard.conf <<EOF
+Section "InputClass"
+        Identifier "system-keyboard"
+        MatchIsKeyboard "on"
+        Option "XkbLayout" "$KB_LAYOUT"
+        Option "XkbModel" "$KB_MAP"
+EndSection
+EOF
+
+mkdir -p $SYSROOT/home/alarm/.config/openbox
+cat > $SYSROOT/home/alarm/.config/openbox/autostart <<EOF 
+kitty &
+onboard &
+EOF
+
+e_status "Adding BCM4354.hcd"
+wget -O $SYSROOT/lib/firmware/brcm/BCM4354.hcd 'https://github.com/denysvitali/linux-smaug/blob/v4.17-rc3/firmware/bcm4354.hcd?raw=true'
+
+e_status "Removing /var/cache/ content"
+rm -rf $SYSROOT/var/cache
+mkdir -p $SYSROOT/var/cache
+
 e_status "RootFS generation done."
+
+unset RFS_WIFI_SSID
+unset RFS_WIFI_PASSWORD
